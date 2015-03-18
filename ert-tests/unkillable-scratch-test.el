435e1497f43df3eb3c607658c8216d9243a14639
Eric Crosson <esc@ericcrosson.com>
2015-03-18 18:26:15 -0500

    test updated to verify general behavior
    meaning the use of variable `unkillable-buffers'

--- ert-tests/unkillable-scratch-test.el
+++ ert-tests/unkillable-scratch-test.el
@@ -3,9 +3,30 @@
 (unkillable-scratch 1)
 
 (ert-deftest test-unkillable-scratch ()
+  ;; test the default '*scratch* only' functionality
   (let ((scratch "*scratch*"))
     (kill-buffer scratch)
     (should (buffer-live-p (get-buffer scratch)))))
+
+(ert-deftest test-unkillable-buffers ()
+  ;; test the general functionality
+  (let ((general "red-shirt"))
+    (get-buffer-create general)
+    (should (buffer-live-p (get-buffer general)))
+    (kill-buffer general)
+    (should (eq nil
+		(let ((retval t))
+		  (catch 'found
+		    (mapc (lambda (buf)
+			    (when (string-match general (buffer-name buf)) (throw 'found nil)))
+			  (buffer-list))
+		    (setq retval nil))
+		  retval)))
+    ;; now add it to the list and test for indestructability
+    (get-buffer-create general)
+    (add-to-list 'unkillable-buffers general)
+    (kill-buffer general)
+    (should (buffer-live-p (get-buffer general)))))
 
 (provide 'unkillable-scratch-test)
 ;;; unkillable-scratch-test.el ends here
---
+++ unkillable-scratch-autoloads.el
@@ -0,0 +1,33 @@
+;;; unkillable-scratch-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "unkillable-scratch" "unkillable-scratch.el"
+;;;;;;  (21770 798 913946 796000))
+;;; Generated autoloads from unkillable-scratch.el
+
+(defvar unkillable-scratch nil "\
+Non-nil if Unkillable-Scratch mode is enabled.
+See the command `unkillable-scratch' for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `unkillable-scratch'.")
+
+(custom-autoload 'unkillable-scratch "unkillable-scratch" nil)
+
+(autoload 'unkillable-scratch "unkillable-scratch" "\
+A minor mode to disallow the *scratch* buffer from being killed.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+(provide 'unkillable-scratch-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; unkillable-scratch-autoloads.el ends here
